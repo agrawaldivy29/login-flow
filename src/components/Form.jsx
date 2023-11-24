9ed9dee35745bc61d6126938fc7aa0e4db48aa6e
Divy Agrawal <divyagrawal29@gmail.com>
2023-11-24 18:57:13 +0530

    Mouse hover colour change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 
 function Form() {
+  const [isMouseOver, setMouseOver] = useState(false);
+  function onMouseOver() {
+    setMouseOver(true);
+  }
+  function onMouseOut() {
+    setMouseOver(false);
+  }
   return (
     <form className="form">
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
+      <button
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+        type="submit"
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+      >
+        Login
+      </button>
     </form>
   );
 }
